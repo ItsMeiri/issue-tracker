dd30a8cedd8c687ed42627e44832eae9143e9f7b
Daniel Meiri <danielt.meiri@gmail.com>
2024-04-09 21:55:36 +0300

    customize Radix UI theme and font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,16 @@
 import "@radix-ui/themes/styles.css";
+import "./theme-config.css";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import NavBar from "./NavBar";
 import { Theme } from "@radix-ui/themes";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({
+  subsets: ["latin"],
+  display: 'swap',
+  variable: "--font-inter"
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -19,9 +24,9 @@ export default function RootLayout({
 }) {
   return (
 
-    <html lang="en">
-    <body className={inter.className}>
-    <Theme>
+    <html lang="en" className={inter.variable}>
+    <body >
+    <Theme accentColor="yellow" grayColor="slate" radius="small">
       <NavBar />
       <main className="p-5">{children}</main>
     </Theme>

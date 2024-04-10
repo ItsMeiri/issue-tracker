2df6e10c85646168c1f4e6584a63de8713c45c2e
Daniel Meiri <danielt.meiri@gmail.com>
2024-04-10 14:50:33 +0300

    handle new issue form submission

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -3,13 +3,18 @@ import { z } from "zod";
 import prisma from "@/prisma/client";
 
 
+
 const createIssueSchema = z.object({
   title: z.string().min(1).max(255),
   description: z.string().min(1)
 });
 
+
 export async function POST(request: NextRequest) {
   const body = await request.json();
+  if (!body) {
+    return NextResponse.json(body.error.errors, { status: 400 });
+  }
   const validation = createIssueSchema.safeParse(body);
   if(!validation.success){
     return NextResponse.json(validation.error.errors, {status: 400});
--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -2,15 +2,37 @@
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
 import { Button, TextField } from "@radix-ui/themes";
+import { Controller, useForm } from "react-hook-form";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+
+interface IssueForm {
+  title: string;
+  description: string;
+}
 
 const NewIssuePage = () => {
-  return (
-    <div className="max-w-xl space-y-3">
-      <TextField.Root placeholder="title"/>
-      <SimpleMDE placeholder="description"/>
-      <Button> Submit new issue</Button>
-    </div>
-  );
-};
+  const { register, control, handleSubmit } = useForm();
+  const router = useRouter();
 
+
+  return <form
+    className="max-w-xl space-y-3" onSubmit={handleSubmit(async (data) => {
+    const axiosResponse = await axios.post("/api/issues", data);
+    console.log(axiosResponse.data);
+    router.push("/issues");
+  })}
+  >
+    <TextField.Root
+      placeholder="title"
+      {...register("title")} />
+    <Controller
+      name="description"
+      control={control}
+      render={({ field }) => <SimpleMDE placeholder="description" {...field} />}
+    />
+    <Button> Submit new issue</Button>
+  </form>;
+};
 export default NewIssuePage;
+

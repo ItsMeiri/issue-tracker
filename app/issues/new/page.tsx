06aa8a574184ac5380e45bfa7ed73e940707833a
Daniel Meiri <danielt.meiri@gmail.com>
2024-04-10 15:27:49 +0300

    implement client side validation for new issue form

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,14 +1,21 @@
 "use client";
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
-import { Button, Callout, TextField } from "@radix-ui/themes";
+import { Button, Callout, TextField, Text } from "@radix-ui/themes";
 import { Controller, useForm } from "react-hook-form";
 import axios from "axios";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { createIssueSchema } from "@/app/validationSchemas";
+import { z } from "zod";
+
+type IssueForm = z.infer<typeof createIssueSchema>
 
 const NewIssuePage = () => {
-  const { register, control, handleSubmit } = useForm();
+  const { register, control, handleSubmit, formState: { errors } } = useForm<IssueForm>({
+    resolver: zodResolver(createIssueSchema)
+  });
   const router = useRouter();
   const [error, setError] = useState("");
 
@@ -17,7 +24,7 @@ const NewIssuePage = () => {
     <form
       className="max-w-xl space-y-3" onSubmit={handleSubmit(async (data) => {
       try {
-        const axiosResponse = await axios.post("/1api/issues", data);
+        const axiosResponse = await axios.post("/api/issues", data);
         console.log(axiosResponse.data);
         router.push("/issues");
       } catch (error) {
@@ -33,11 +40,14 @@ const NewIssuePage = () => {
       <TextField.Root
         placeholder="title"
         {...register("title")} />
+      {errors.title && <Text color={"red"} as={"p"}>{errors.title.message}</Text>}
       <Controller
         name="description"
         control={control}
         render={({ field }) => <SimpleMDE placeholder="description" {...field} />}
       />
+      {errors.description && <Text color={"red"} as={"p"}>{errors.description.message}</Text>}
+
       <Button> Submit new issue</Button>
     </form>
   </>;

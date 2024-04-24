2e24593b35c24a410e3f0fbe3d5d88481a1d7e67
Daniel Meiri <danielt.meiri@gmail.com>
2024-04-24 19:11:37 +0300

    add spinner to delete issue button

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -3,15 +3,34 @@ import { AlertDialog, Button, Flex } from "@radix-ui/themes";
 import axios from "axios";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
+import { Spinner } from "@/app/components";
 
 const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
   const router = useRouter();
   const [error, setError] = useState(false);
+  const [isDeleting, setIsDeleting] = useState(false);
+
+  async function handleDelete() {
+    try {
+      setError(false);
+      setIsDeleting(true);
+      await axios.delete(`/api/issues/${issueId}`);
+      router.push("/issues");
+      router.refresh();
+    } catch (e) {
+      setIsDeleting(false);
+      setError(true);
+      console.log(e);
+    }
+  }
+
   return (
     <>
       <AlertDialog.Root>
         <AlertDialog.Trigger>
-          <Button color="red">Delete Issue</Button>
+          <Button color="red" disabled={isDeleting}>
+            Delete Issue
+          </Button>
         </AlertDialog.Trigger>
         <AlertDialog.Content>
           <AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
@@ -22,6 +41,7 @@ const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
             <AlertDialog.Action>
               <Button color={"red"} onClick={handleDelete}>
                 Delete Issue
+                {isDeleting && <Spinner />}
               </Button>
             </AlertDialog.Action>
             <AlertDialog.Cancel>
@@ -51,17 +71,6 @@ const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
       </AlertDialog.Root>
     </>
   );
-
-  async function handleDelete() {
-    try {
-      await axios.delete(`/api/issues/${issueId}`);
-      router.push("/issues");
-      router.refresh();
-    } catch (e) {
-      setError(true);
-      console.log(e);
-    }
-  }
 };
 
 export default DeleteIssueButton;

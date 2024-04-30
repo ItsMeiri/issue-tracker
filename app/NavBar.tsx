1461233f360a9caa8f5a7a8199add7a37c219e36
Daniel Meiri <danielt.meiri@gmail.com>
2024-04-30 12:19:00 +0300

    update navbar layout

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -6,7 +6,7 @@ import React from "react";
 import { IoBug } from "react-icons/io5";
 import classnames from "classnames";
 import { useSession } from "next-auth/react";
-import { Box } from "@radix-ui/themes";
+import { Box, Container, Flex } from "@radix-ui/themes";
 
 const NavBar = () => {
   const path = usePathname();
@@ -14,38 +14,42 @@ const NavBar = () => {
 
   const links = [
     { href: "/", label: "Dashboard" },
-    { href: "/issues/list", label: "Issues" },
+    { href: "/issues/list", label: "Issues" }
   ];
 
   return (
-    <nav className="mb-5 flex h-14 items-center space-x-6 border-b px-4 ">
-      <Link href="/">
-        <IoBug />
-      </Link>
-      <ul className="flex space-x-6">
-        {links.map((link) => (
-          <li key={link.href}>
-            <Link
-              className={classnames({
-                "text-zinc-900": path === link.href,
-                "text-zinc-500": path !== link.href,
-                "transition-colors hover:text-zinc-800": true,
-              })}
-              href={link.href}
-            >
-              {link.label}
-            </Link>
-          </li>
-        ))}
-      </ul>
-      <Box>
-        {status === "authenticated" && (
-          <Link href="http://localhost:3000/api/auth/signout">Sign out</Link>
-        )}
-        {status === "unauthenticated" && (
-          <Link href={`http://localhost:3000/api/auth/signin`}>Sign in</Link>
-        )}
-      </Box>
+    <nav className="mb-5  border-b   px-4 py-3 ">
+      <Container><Flex justify={"between"}>
+        <Flex align={"center"} gap={"3"}>
+          <Link href="/">
+            <IoBug />
+          </Link>
+          <ul className="flex space-x-6">
+            {links.map((link) => (
+              <li key={link.href}>
+                <Link
+                  className={classnames({
+                    "text-zinc-900": path === link.href,
+                    "text-zinc-500": path !== link.href,
+                    "transition-colors hover:text-zinc-800": true
+                  })}
+                  href={link.href}
+                >
+                  {link.label}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </Flex>
+        <Box>
+          {status === "authenticated" && (
+            <Link href="http://localhost:3000/api/auth/signout">Sign out</Link>
+          )}
+          {status === "unauthenticated" && (
+            <Link href={`http://localhost:3000/api/auth/signin`}>Sign in</Link>
+          )}
+        </Box>
+      </Flex></Container>
     </nav>
   );
 };
